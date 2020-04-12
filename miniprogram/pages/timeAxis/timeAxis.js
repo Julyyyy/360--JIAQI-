2deed6e371e0ae89715fc0f6beebc9a5ef317a38
zhaoyin <zhaoyin@neotel.com.cn>
2020-04-12 23:21:14 +0800

    timeAxis init

--- miniprogram/pages/timeAxis/timeAxis.js
+++ miniprogram/pages/timeAxis/timeAxis.js
@@ -1,18 +1,341 @@
-// pages/timeAxis.js
+// miniprogram/pages/timeAxis.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    data: [],
+    dayDate: [],
+    activeIndex: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const db = wx.cloud.database()
+    // db.collection('user').where({
+    //   name: 'xxx'
+    // }).get().then(res => {
+    //   console.log(res)
+    // })
 
+    // db.collection('user').where({
+    //   name: 'xxx'
+    // }).update({
+    //   data: {
+    //     name: 'hhh'
+    //   },
+    //   success: data => {
+    //     console.log(data);
+    //   }
+    // })
+
+    // db.collection('user').where({
+    //   name: 'xxx'
+    // }).update({
+    //   data: {
+    //     name: 'dddd'
+    //   },
+    //   success: function(res) {
+    //     console.log('res.data')
+    //     console.log(res)
+    //   }
+    // })
+    
+    this.setData({
+      data: [
+        {
+          day: '2020/04/12',
+          msg: [
+            {
+              time: '01:00:00',
+              anchor: {
+                name: 'xhy1',
+                setTime: '01:00:00',
+              }
+            },
+            {
+              time: '05:00:00',
+              anchor: {
+                name: 'xhy2',
+              }
+            },
+            {
+              time: '11:00:00',
+              anchor: {
+                name: 'xhy3',
+                setTime: '01:00:00',
+              }
+            },
+            {
+              time: '01:00:00',
+              anchor: {
+                name: 'xhy1',
+                setTime: '01:00:00',
+              }
+            },
+            {
+              time: '05:00:00',
+              anchor: {
+                name: 'xhy2',
+              }
+            },
+            {
+              time: '11:00:00',
+              anchor: {
+                name: 'xhy3',
+                setTime: '01:00:00',
+              }
+            },
+            {
+              time: '01:00:00',
+              anchor: {
+                name: 'xhy1',
+                setTime: '01:00:00',
+              }
+            },
+            {
+              time: '05:00:00',
+              anchor: {
+                name: 'xhy2',
+              }
+            },
+            {
+              time: '11:00:00',
+              anchor: {
+                name: 'xhy3',
+                setTime: '01:00:00',
+              }
+            },
+            {
+              time: '01:00:00',
+              anchor: {
+                name: 'xhy1',
+                setTime: '01:00:00',
+              }
+            },
+            {
+              time: '05:00:00',
+              anchor: {
+                name: 'xhy2',
+              }
+            },
+            {
+              time: '11:00:00',
+              anchor: {
+                name: 'xhy3',
+                setTime: '01:00:00',
+              }
+            },
+            {
+              time: '01:00:00',
+              anchor: {
+                name: 'xhy1',
+                setTime: '01:00:00',
+              }
+            },
+            {
+              time: '05:00:00',
+              anchor: {
+                name: 'xhy2',
+              }
+            },
+            {
+              time: '11:00:00',
+              anchor: {
+                name: 'xhy3',
+                setTime: '01:00:00',
+              }
+            },
+          ]
+        },
+        {
+          day: '2020/04/13',
+          msg: [
+            {
+              time: '01:00:00',
+              anchor: {
+                name: 'xhy13333',
+              }
+            },
+            {
+              time: '05:00:00',
+              anchor: {
+                name: 'xhy2',
+              }
+            },
+            {
+              time: '11:00:00',
+              anchor: {
+                name: 'xhy3',
+              }
+            },
+          ]
+        },
+        {
+          day: '2020/04/14',
+          msg: [
+            {
+              time: '01:00:00',
+              anchor: {
+                name: 'xhy144444',
+              }
+            },
+            {
+              time: '05:00:00',
+              anchor: {
+                name: 'xhy2',
+              }
+            },
+            {
+              time: '11:00:00',
+              anchor: {
+                name: 'xhy3',
+              }
+            },
+          ]
+        },
+        {
+          day: '2020/04/15',
+          msg: [
+            {
+              time: '01:00:00',
+              anchor: {
+                name: 'xhy15555',
+              }
+            },
+            {
+              time: '05:00:00',
+              anchor: {
+                name: 'xhy2',
+              }
+            },
+            {
+              time: '11:00:00',
+              anchor: {
+                name: 'xhy3',
+              }
+            },
+          ]
+        },
+      ],
+      dayDate: [
+        {
+          time: '01:00:00',
+          anchor: {
+            name: 'xhy1',
+            setTime: '01:00:00',
+          }
+        },
+        {
+          time: '05:00:00',
+          anchor: {
+            name: 'xhy2',
+          }
+        },
+        {
+          time: '11:00:00',
+          anchor: {
+            name: 'xhy3',
+            setTime: '01:00:00',
+          }
+        },
+        {
+          time: '01:00:00',
+          anchor: {
+            name: 'xhy1',
+            setTime: '01:00:00',
+          }
+        },
+        {
+          time: '05:00:00',
+          anchor: {
+            name: 'xhy2',
+          }
+        },
+        {
+          time: '11:00:00',
+          anchor: {
+            name: 'xhy3',
+            setTime: '01:00:00',
+          }
+        },
+        {
+          time: '01:00:00',
+          anchor: {
+            name: 'xhy1',
+            setTime: '01:00:00',
+          }
+        },
+        {
+          time: '05:00:00',
+          anchor: {
+            name: 'xhy2',
+          }
+        },
+        {
+          time: '11:00:00',
+          anchor: {
+            name: 'xhy3',
+            setTime: '01:00:00',
+          }
+        },
+        {
+          time: '01:00:00',
+          anchor: {
+            name: 'xhy1',
+            setTime: '01:00:00',
+          }
+        },
+        {
+          time: '05:00:00',
+          anchor: {
+            name: 'xhy2',
+          }
+        },
+        {
+          time: '11:00:00',
+          anchor: {
+            name: 'xhy3',
+            setTime: '01:00:00',
+          }
+        },
+        {
+          time: '01:00:00',
+          anchor: {
+            name: 'xhy1',
+            setTime: '01:00:00',
+          }
+        },
+        {
+          time: '05:00:00',
+          anchor: {
+            name: 'xhy2',
+          }
+        },
+        {
+          time: '11:00:00',
+          anchor: {
+            name: 'xhy3',
+            setTime: '01:00:00',
+          }
+        },
+      ]
+    })
+    console.log(this.data.data)
+  },
+
+  changeDate(e) {
+    this.setData({
+      dayDate: e.currentTarget.dataset.itemdata.msg,
+      activeIndex: e.currentTarget.dataset.indexdata
+    })
+  },
+
+  bindTimeChange(e) {
+    console.log(e);
   },
 
   /**
